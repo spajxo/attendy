1c79eb6ded7d15b096214a1a8d1b2e13c5504d20
spajx <spajxo@gmail.com>
2017-08-20 16:08:45 +0200

    [WIP] WIP

---
+++ src/AppBundle/Controller/TimeCardController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\TimeCard;
+use AppBundle\Entity\TimeEntry;
+use DateTime;
+use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class TimeCardController
+ * @package AppBundle\Controller
+ */
+class TimeCardController extends FOSRestController
+{
+
+    /**
+     * @param Request $request
+     * @return array
+     * @Route("timecard/", name="timecard_index")
+     * @Template()
+     */
+    public function indexAction(Request $request): array
+    {
+        $monthString = $request->get('month') ?: 'midnight first day of this month';
+        $month = new DateTime($monthString);
+
+        $repo = $this->getDoctrine()->getRepository(TimeEntry::class);
+        $timeEntries = $repo->findAllInMonth($month);
+
+        $timeCard = new TimeCard($month, $timeEntries);
+
+        return [
+            'month' => $month,
+            'timeCard' => $timeCard,
+        ];
+    }
+
+    public function getTimecard()
+    {
+
+    }
+
+}
